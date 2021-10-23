e57455fcc8c171d051548171c5882806ebb29e88
kkmalhotra <49594530+kkmalhotra@users.noreply.github.com>
2021-10-24 00:53:15 +0530

    Add files via upload

---
+++ checkout.js
@@ -0,0 +1,22 @@
+function gotohome(){
+		document.getElementsByClassName("b3")
+		window.location.href = "find_doctor.html";
+		
+	}
+   
+    function gotocart(){
+		document.getElementsByClassName("previous")
+		window.location.href = "cart_detail.html";
+		
+	}
+    function gotomedicines()
+    {
+        document.getElementsByClassName("b2")
+		window.location.href = "medicines.html";
+    }
+
+    function gotopayment()
+    {
+        document.getElementsByClassName("b3")
+		window.location.href = "payment_page.html";
+    }
